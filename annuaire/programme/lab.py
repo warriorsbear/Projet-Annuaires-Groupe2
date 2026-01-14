f62614fbfa82e19200f3475854b7cd7f3c8fd380
Nicolas Ruiz <nicolas.ruiz2075@gmail.com>
2026-01-14 14:11:04 +0100

    supp inutile

--- annuaire/programme/lab.py
+++
@@ -1,3 +0,0 @@
-import hashlib
-mdp = hashlib.sha256("123456".encode()).hexdigest()
-print(mdp)
--- annuaire/programme/main.py
+++ annuaire/programme/main.py
@@ -644,14 +644,5 @@ def main() -> None:
 
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     main()
